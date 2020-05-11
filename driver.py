f464ca5e2da090b827b9faadd041c64609af29b5
wzhou2 <wzhou2@stuy.edu>
2020-05-11 03:10:18 -0400

    input from a file

--- driver.py
+++ driver.py
@@ -1,5 +1,22 @@
 from FSA import *
 
+
+def read_file(path):
+    """ Returns the fsa defined by the input file """
+    f = open(path)
+    lines = f.readlines()
+    f.close()
+    # first 2 lines are initial and final states
+    I = lines[0].strip().split(" ")
+    F = lines[1].strip().split(" ")
+    # the rest are transition relations
+    T = []
+    for line in lines[2:]:
+        # only the first 3 are looked at
+        cur, l, next = tuple(line.strip().split(" ")[:3])
+        T.append((cur, l, next))
+
+    return FSA(I, F, T)
 # To define your FSA
 
 # You can play around with these variables.
@@ -11,7 +28,7 @@ T = [   ("X", "a", "Y"),
         ("Y", "b", "Z"),
         ("Z", "a", "X")] # Transition relations
 
-my_fsa = FSA(I, F, T)
+my_fsa = read_file("INPUT/example.txt")
 
 words_to_test = ["aab", "aba", "a", "abaa", ""]
 # should be [False, False, True, True, False]
